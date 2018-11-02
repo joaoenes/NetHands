62d3969935415f7ed92a842eeb2ffacc58947594
joaoenes <joaofenes@gmail.com>
2018-11-02 15:47:18 -0100

    Added prompt GameMenu

--- src/org/academiadecodigo/bootcamp/Client/Messages.java
+++ src/org/academiadecodigo/bootcamp/Client/Messages.java
@@ -5,4 +5,7 @@ public class Messages {
     public static final String WELCOME = "Welcome to Net Hands";
     public static final String LOGIN = "Enter your username and password";
 
+    public static final String ROCK = "Rock";
+    public static final String PAPER = "Paper";
+    public static final String SCISSORS = "Scissors";
 }
--- src/org/academiadecodigo/bootcamp/Client/PromptView.java
+++ src/org/academiadecodigo/bootcamp/Client/PromptView.java
@@ -1,12 +1,14 @@
 package org.academiadecodigo.bootcamp.Client;
 
 import org.academiadecodigo.bootcamp.Prompt;
+import org.academiadecodigo.bootcamp.scanners.menu.MenuInputScanner;
 
 public class PromptView {
 
-    public void showWelcome() {
-        Prompt prompt = new Prompt(System.in, System.out);
+    Prompt prompt = new Prompt(System.in, System.out);
 
+    public void showGameMenu() {
 
+        MenuInputScanner menu = new MenuInputScanner()
     }
 }
