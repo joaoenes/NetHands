be70bcb0048dd5e36cdd992f4a3c8ba7ebd30838
Luis Rodrigues <luismanuelsilvarodrigues@gmail.com>
2018-11-02 15:08:30 -0100

    Server Logic + PollThread finished

--- src/org/academiadecodigo/bootcamp/Server/Server.java
+++ src/org/academiadecodigo/bootcamp/Server/Server.java
@@ -1,8 +1,65 @@
 package org.academiadecodigo.bootcamp.Server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Server {
 
-    public static void main(String[] args) {
+    private ServerSocket serverSocket;
+    private ExecutorService cachedPool;
+    private Socket clientSocket;
 
+    public Server() {
+        cachedPool = Executors.newCachedThreadPool();
+    }
+
+    public void run() {
+        while (true) {
+
+            try {
+                
+                clientSocket = serverSocket.accept();
+                newThread(clientSocket);
+                clientSocket.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public void init() {
+        try {
+            System.out.print("PORT: ");
+            serverSocket = new ServerSocket(sacanner());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int sacanner() {
+        Scanner scanner = new Scanner(System.in);
+        int port = Integer.parseInt(scanner.nextLine());
+        return port;
+    }
+
+    private void newThread(Socket clientSocket) {
+        cachedPool.submit(new Runnable() {
+            @Override
+            public void run() {
+                new Client(clientSocket);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.init();
+        server.run();
     }
 }
