b6f1186edfe1dfdc7c3d3434131198d656662a87
Tiago Ormonde <tnm.ormonde@gmail.com>
2018-11-03 17:43:35 -0100

    Improved code

--- src/org/academiadecodigo/bootcamp/server/Score.java
+++ src/org/academiadecodigo/bootcamp/server/Score.java
@@ -10,84 +10,79 @@ public class Score {
     private static final String ROOT_PATH = "resources/";
     private static final File file = new File(ROOT_PATH + PATH_SCORELOG);
 
-    public static String readScore(String clientName) {
-        synchronized (Game.class) {
-            BufferedReader reader = null;
-            int score = 0;
+    public static synchronized String readScore(String clientName) {
+        BufferedReader reader = null;
+        int score = 0;
 
-            try {
+        try {
 
-                reader = new BufferedReader(new FileReader(file));
+            reader = new BufferedReader(new FileReader(file));
 
-                String line = "";
-                String[] lineWords;
+            String line = "";
+            String[] lineWords;
 
-                while ((line = reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
 
-                    lineWords = line.split(Messages.ESCAPE_TAG);
+                lineWords = line.split(Messages.ESCAPE_TAG);
 
-                    if (lineWords[0].equals(clientName)) {
-                        switch (lineWords[1]) {
-                            case Messages.TIE_LOG:
-                                score += 1;
-                                break;
-                            case Messages.WON_LOG:
-                                score += 2;
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-                }
-
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                if (lineWords[0].equals(clientName)) {
+                    switch (lineWords[1]) {
+                        case Messages.TIE_LOG:
+                            score += 1;
+                            break;
+                        case Messages.WON_LOG:
+                            score += 2;
+                            break;
+                        default:
+                            break;
                     }
                 }
             }
 
-            return clientName + " " + score;
 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
+        return clientName + " " + score;
 
     }
 
-    public static void saveLog(String line) {
+    public static synchronized void saveLog(String line) {
 
-        synchronized (Game.class) {
-            FileOutputStream output = null;
+        FileOutputStream output = null;
 
-            try {
+        try {
 
-                output = new FileOutputStream(file);
+            output = new FileOutputStream(file);
 
-                output.write(line.getBytes());
+            output.write(line.getBytes());
 
-                output.flush();
+            output.flush();
 
-                output.close();
+            output.close();
 
-            } catch (IOException e) {
+        } catch (IOException e) {
 
-                e.printStackTrace();
+            e.printStackTrace();
 
-            } finally {
+        } finally {
 
-                if (output != null) {
-                    try {
-                        output.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         }
