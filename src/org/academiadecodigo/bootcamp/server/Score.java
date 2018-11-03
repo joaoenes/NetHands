d74492ccad5005f0144b5694818d65f783e56382
Tiago Ormonde <tnm.ormonde@gmail.com>
2018-11-03 17:07:19 -0100

    Created the method to read the scoreLog and return current client score

--- src/org/academiadecodigo/bootcamp/server/Score.java
+++ src/org/academiadecodigo/bootcamp/server/Score.java
@@ -1,5 +1,7 @@
 package org.academiadecodigo.bootcamp.server;
 
+import org.academiadecodigo.bootcamp.messages.Messages;
+
 import java.io.*;
 
 public class Score {
@@ -8,10 +10,55 @@ public class Score {
     private static final String ROOT_PATH = "resources/";
     private static final File file = new File(ROOT_PATH + PATH_SCORELOG);
 
-    public static void readScore(){
-        synchronized (Game.class){
+    public static String readScore(String clientName) {
+        synchronized (Game.class) {
+            BufferedReader reader = null;
+            int score = 0;
+
+            try {
+
+                reader = new BufferedReader(new FileReader(file));
+
+                String line = "";
+                String[] lineWords;
+
+                while ((line = reader.readLine()) != null) {
+
+                    lineWords = line.split(Messages.ESCAPE_TAG);
+
+                    if (lineWords[0].equals(clientName)) {
+                        switch (lineWords[1]) {
+                            case Messages.TIE_LOG:
+                                score += 1;
+                                break;
+                            case Messages.WON_LOG:
+                                score += 2;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            return clientName + " " + score;
 
         }
+
     }
 
     public static void saveLog(String line) {
@@ -33,9 +80,9 @@ public class Score {
 
                 e.printStackTrace();
 
-            }finally {
+            } finally {
 
-                if(output != null){
+                if (output != null) {
                     try {
                         output.close();
                     } catch (IOException e) {
@@ -45,6 +92,5 @@ public class Score {
             }
         }
     }
-
 
 }
